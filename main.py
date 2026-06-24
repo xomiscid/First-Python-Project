dafc6203482ea6b44092de3c12398ec82ae8aa2b
乂ㄖ爪 <amabouak@gmail.com>
2026-06-24 21:04:37 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+#First python project
+
+print("== HERO 1 INFORMATIONS ==")
+print("Name :","John")
+print("Power :", "Water")
+print("HP :", 100)
+
+#Other method : with variables
+print()
+
+print("== HERO 2 INFORMATIONS ==")
+
+name = "Edouard"
+power = "Fire"
+hp = 95
+
+print("Name :",name)
+print("Power :",power)
+print("HP :",hp)
